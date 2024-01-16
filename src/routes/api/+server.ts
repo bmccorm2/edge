536435de7b41d0b993141ba28a256b3ea2a6c711
Bryan McCormick <bryan@1mccormick.com>
2024-01-15 18:46:53 -0700

    a

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,8 +1,8 @@
-import type { Config } from "@sveltejs/adapter-vercel";
+// import type { Config } from "@sveltejs/adapter-vercel";
 
-export const config: Config = {
-  runtime: "edge",
-};
+// export const config: Config = {
+//   runtime: "edge",
+// };
 
 export const GET = async () => {
   return new Response(`Hello world! - ${process.env.VERCEL_REGION}`);
